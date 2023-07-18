9823831f1b3b7d58a981a7ae13df100265a004c3
Chiahao <chiahao.codes@gmail.com>
2023-07-17 20:52:46 -0700

    main

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //if KeyboardEvent.key = " ", remove the whitespace.
 
 let tickerText = document.querySelector("header h2");
+console.log(tickerText);
 tickerText.addEventListener("click", () => {
-    
+
     if (tickerText.innerText === "Enter ticker...") {
         tickerText.innerText = "";
     }
