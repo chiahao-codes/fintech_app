9201e323f3755931e93d785e9522988525ea3949
Chiahao <chiahao.codes@gmail.com>
2023-07-19 09:18:57 -0700

    commit main

--- script.js
+++ script.js
@@ -11,25 +11,11 @@ h2Box.addEventListener("click", () => {
 })
 
 h2Box.addEventListener("keydown", (e) => {
-    let h2Children = h2Box.children;
-    let arrCopy = Array.from(h2Children);
     
     if (regExp.test(e.key)) {
         e.preventDefault();
         alert("Invalid entry")
     }
 
-    if (arrCopy.length > 0) {
-        for (let i = 0; i < arrCopy.length; i++) {
-            let curr = arrCopy[i];
-            if (curr.outerHTML == "<br>") {
-                arrCopy.splice(i, 1);
-            }
-        }
-       
-        console.log("<br> deleted...")
-    }
-    console.log(h2Box);
-    h2Children = arrCopy;
-    return h2Children;
+    return 
 });
