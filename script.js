48f6437064087a515e28b492cf48f621dae01b0c
Chiahao <chiahao.codes@gmail.com>
2023-07-19 15:00:45 -0700

    commit main

--- script.js
+++ script.js
@@ -9,30 +9,35 @@ h2Box.addEventListener("click", () => {
 });
 
 h2Box.addEventListener("keydown", (e) => {
+  let h2Children = h2Box.children;
   if (regExp.test(e.key)) {
     e.preventDefault();
     alert("Invalid entry");
   }
-  return;
-});
 
-//mozilla hack for getting rid of <br>;
-h2Box.addEventListener("keyup", (e) => {
-  //remove <br>
-  //let brNodeList = document.querySelectorAll("h2 > br");
-    let h2Children = h2Box.children;
-
-    if ((e.key == "Backspace" && h2Children.length > 0) || (e.key == "Enter" && h2Children.length > 0)) {
-        for (let i = 0; i < h2Children.length; i++){
-            h2Children[i].remove(i);
-        }
+  if (
+    (e.key == "Backspace" && h2Children.length > 0) ||
+    (e.key == "Enter" && h2Children.length > 0)
+  ) {
+    for (let i = 0; i < h2Children.length; i++) {
+      h2Children[i].remove(i);
+    }
     console.log(h2Box);
   }
   return;
 });
 
+//mozilla hack for getting rid of <br>;
+
 /**
- *    brNodeList.forEach((ele) => {
+ *   
+ * h2Box.addEventListener("keyup", (e) => {
+  //remove <br>
+  //let brNodeList = document.querySelectorAll("h2 > br");
+
+  return;
+});
+ *  brNodeList.forEach((ele) => {
       if (ele.outerHTML === "<br>") {
         ele.remove();
       }
