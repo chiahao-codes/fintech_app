5d94169095f18389eba0efceeb098e51a04ced71
Chiahao <chiahao.codes@gmail.com>
2023-07-19 14:44:07 -0700

    commit main

--- script.js
+++ script.js
@@ -2,45 +2,41 @@ let regExp = /[^a-z.A-Z]/;
 let h2Box = document.querySelector("header h2");
 
 h2Box.addEventListener("click", () => {
-    if (h2Box.innerText === "Enter ticker...") {
-        h2Box.innerText = "";
-    }
-    return
-})
+  if (h2Box.innerText === "Enter ticker...") {
+    h2Box.innerText = "";
+  }
+  return;
+});
 
 h2Box.addEventListener("keydown", (e) => {
-    if (regExp.test(e.key)) {
-        e.preventDefault();
-        alert("Invalid entry")
-    }
-        return; 
+  if (regExp.test(e.key)) {
+    e.preventDefault();
+    alert("Invalid entry");
+  }
+  return;
 });
 
 //mozilla hack for getting rid of <br>;
 h2Box.addEventListener("keyup", (e) => {
-    //remove <br>
-    let brNodeList = document.querySelectorAll("h2 > br");
-    
-    if (e.key == "Backspace" && brNodeList.length>0||e.key == "Enter" && brNodeList.length > 0) {
-        brNodeList.forEach((ele) => {
-            if (ele.outerHTML === "<br>") {
-                ele.remove()
+  //remove <br>
+  let brNodeList = document.querySelectorAll("h2 > br");
+
+    if ((e.key == "Backspace" && brNodeList.length > 0) || (e.key == "Enter" && brNodeList.length > 0)) {
+        for (let i = 0; i < brNodeList.length; i++){
+            let curr = brNodeList[i];
+            if (curr.outerHTML === "<br>") {
+              curr.remove();
             }
-        })
-        console.log(h2Box)
-    }
-        return;
-})
+        }
+    console.log(h2Box);
+  }
+  return;
+});
 
 /**
- * let brElement = document.querySelector("h2 br");
- *  if ( brElement) {
-    brElement.remove();
-    console.log(h2Box);
-    }
- * let brElem = document.querySelector("h2 br");
- *   if (e.key == "Backspace" && brElement || e.key === "Enter" && brElement) {
-        brElement.remove();
-        console.log(h2Box)
-    }
+ *    brNodeList.forEach((ele) => {
+      if (ele.outerHTML === "<br>") {
+        ele.remove();
+      }
+    });
  */
