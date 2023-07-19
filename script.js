42a2404333f6b1bbf5c1df4a431fefa44254080d
Chiahao <chiahao.codes@gmail.com>
2023-07-19 14:51:37 -0700

    commit main

--- script.js
+++ script.js
@@ -19,14 +19,12 @@ h2Box.addEventListener("keydown", (e) => {
 //mozilla hack for getting rid of <br>;
 h2Box.addEventListener("keyup", (e) => {
   //remove <br>
-  let brNodeList = document.querySelectorAll("h2 > br");
+  //let brNodeList = document.querySelectorAll("h2 > br");
+    let h2Children = h2Box.children;
 
-    if ((e.key == "Backspace" && brNodeList.length > 0) || (e.key == "Enter" && brNodeList.length > 0)) {
-        for (let i = 0; i < brNodeList.length; i++){
-            let curr = brNodeList[i];
-            if (curr.outerHTML === "<br>") {
-              curr.remove();
-            }
+    if ((e.key == "Backspace" && h2Children.length > 0) || (e.key == "Enter" && h2Children.length > 0)) {
+        for (let i = 0; i < h2Children.length; i++){
+            h2Children[i].remove();
         }
     console.log(h2Box);
   }
