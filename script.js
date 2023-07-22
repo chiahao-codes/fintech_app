ea05055b9647bf934c7c24d1a8d93a8814034376
Chiahao <chiahao.codes@gmail.com>
2023-07-21 20:32:20 -0700

    commit main

--- script.js
+++ script.js
@@ -22,19 +22,24 @@ h2Box.addEventListener("keydown", (e) => {
     e.preventDefault();
     alert("Invalid entry");
   }
-  //prevent consecutive dots to be entered.
-  if (regExpDots.test(h2Box.innerText) === true) {
-    e.preventDefault();
-    alert("invalid entry:'.'");
-  }
-  
+
+   let h2Children = h2Box.children;
+   //remove <br> or div;
+   if (e.key === "Backspace" || e.key === "Enter") {
+     if (h2Children.length > 0) {
+       for (let i = 0; i < h2Children.length; i++) {
+         h2Children[i].remove();
+       }
+     }
+   }
+
   return;
 });
 
 h2Box.addEventListener("keyup", (e) => {
   let h2Children = h2Box.children;
   //remove <br> or div;
-  if ((e.key === "Backspace" || e.key === "Enter")) {
+  if (e.key === "Backspace" || e.key === "Enter") {
     if (h2Children.length > 0) {
       for (let i = 0; i < h2Children.length; i++) {
         h2Children[i].remove();
@@ -42,6 +47,11 @@ h2Box.addEventListener("keyup", (e) => {
     }
   }
 
+  //prevent consecutive dots to be entered.
+  if (regExpDots.test(h2Box.innerText) === true) {
+    e.preventDefault();
+    alert("invalid entry:'.'");
+  }
   return;
 });
 
