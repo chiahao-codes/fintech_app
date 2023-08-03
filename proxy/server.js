2e849c4645ee25171f2bcb2b0667eee469f58c96
Chiahao <chiahao.codes@gmail.com>
2023-08-03 16:12:48 -0400

    commit main

--- proxy/server.js
+++ proxy/server.js
@@ -4,7 +4,5 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-console.log(process.env.SECRET_KEY);
-
 app.use(cors());
 app.listen(PORT, () => { console.log("Proxy listening on port:", PORT) });
