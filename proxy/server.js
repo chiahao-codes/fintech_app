8c13259de3d38bbc56698fb8d635c3bffc990df3
Chiahao <chiahao.codes@gmail.com>
2023-08-16 20:06:43 -0700

    commit main

--- proxy/server.js
+++ proxy/server.js
@@ -1,11 +1,14 @@
 //allow for env variables;
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.get("/", (req, res, next) => {
+  res.send("Running...")
+})
 app.listen(PORT, () => {
     
     console.log("Proxy listening on port:", PORT);
