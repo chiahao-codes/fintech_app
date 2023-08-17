c18db9903cb557206c2710385bfb40be3922fe79
Chiahao <chiahao.codes@gmail.com>
2023-08-16 19:40:49 -0700

    commit main

--- proxy/server.js
+++ proxy/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
