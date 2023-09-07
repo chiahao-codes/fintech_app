25eb03302709c30f38d4ba0ad7183535d1b3947a
Chiahao <chiahao.codes@gmail.com>
2023-09-06 23:37:54 -0700

    commit main

--- proxy/server.js
+++ proxy/server.js
@@ -1,23 +1,32 @@
 //allow for env variables;
 require('dotenv').config();
 const express = require('express');
-//const yahoo2 = require('yahoo-finance2').default;
-const { financials } = require('barchart-dot-com');
+const { financials, quotes } = require('barchart-dot-com');
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static("public"));
+app.set("views", "./views");
+app.set("view engine", 'ejs');
+
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 })
 
+app.get('/', (req, res, next) => {
+  res.render('index');
+  next();
+})
+
 
 app.get("/stock", async (req, res, next) => {
-  let quote;
-  console.log(quote);
-  res.json(quote);
+  let results = await financials.income('ABNB').annual();
+  let quote = await quotes.overview('ABNB');
+  console.log(results);
+  res.json(results);
   next();
 })
 
