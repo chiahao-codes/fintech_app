f51f0d1f2081b5d150d85221b91ae21d080972e2
Chiahao <chiahao.codes@gmail.com>
2023-09-12 22:25:19 -0700

    commit main

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const regExp = /[a-zA-Z]/;
 const h2Box = document.querySelector("header>h2");
 
-h2Box.addEventListener("focusin", () => {
+h2Box.addEventListener("click", () => {
   const h2ChildNodes = h2Box.childNodes;
   const selection = window.getSelection();
   if (h2Box.innerText === "Enter ticker...") {
@@ -10,7 +10,6 @@ h2Box.addEventListener("focusin", () => {
   //set caret position after text node
 
   if (h2ChildNodes.length > 0) {
-    console.log(h2ChildNodes);
     selection.setPosition(h2ChildNodes[0], h2ChildNodes[0].length);
   } else {
     selection.setPosition(h2Box, 0);
